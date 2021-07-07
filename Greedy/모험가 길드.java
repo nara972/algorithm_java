bab0cbbceaa8e7012f992a3b1f7fafd05d162078
nara972 <68370958+nara972@users.noreply.github.com>
2021-07-08 02:43:54 +0900

    Create greedy1.java

---
+++ Greedy/모험가 길드.java
@@ -0,0 +1,36 @@
+package greedy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class greedy1 {
+	
+	public static int n;
+	public static ArrayList<Integer> arrayList=new ArrayList<Integer>();
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("모험가의 수를 입력해주세요:");
+		n=sc.nextInt();
+		
+		for(int i=0;i<n;i++) {
+			arrayList.add(sc.nextInt());
+		}
+		
+		Collections.sort(arrayList);
+		
+		int result=0;//총 그룹의 수
+		int count=0;//현재 그룹에 포함된 모험가의 수
+		
+		for(int i=0;i<n;i++) { //공포도가 낮은 것부터 하나씩 확인하며
+			count+=1;//현재 그룹에 해당 모험가를 포함시키기
+			if(count>=arrayList.get(i)) {//현재 그룹에 포함된 모험가의 수가 현재의 공포도 이상이라면, 그룹 결성
+				result+=1;//총 그룹의 수 증가시키기
+				count=0;//현재 그룹에 포함된 모함가의 수 초기화
+			}
+		}
+		System.out.println(result);
+	}
+
+}
