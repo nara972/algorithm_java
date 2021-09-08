a15c8f2c214377dba183a2b4ae241c63ad1dab2b
nara972 <68370958+nara972@users.noreply.github.com>
2021-09-08 16:16:04 +0900

    Update 완주하지못한선수.java

--- Hash/완주하지못한선수.java
+++ Hash/완주하지못한선수.java
@@ -10,30 +10,15 @@ public class Solution {
 
     public static String solution(String[] participant, String[] completion) {
         String answer = "";
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
 
-        for (String p : participant) {
-            if (map.get(p) == null) {
-                map.put(p, 1);
-            } else {
-                map.put(p, map.get(p) + 1);
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
             }
         }
-
-        for (String p : completion) {
-            if (map.get(p) != 0) {
-                map.put(p, map.get(p) - 1);
-            }
-        }
-
-        for (String key : map.keySet()) {
-            if (map.get(key) != 0) {
-                System.out.println(key);
-                return key;
-            }
-        }
-
         return answer;
-
     }
 }
