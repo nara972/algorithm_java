2df3b3ef956c6fdfed76c0ee9202b6d28088239d
nara972 <68370958+nara972@users.noreply.github.com>
2021-11-01 23:35:47 +0900

    Update K번째 수.java

--- 정렬/K번째 수.java
+++ 정렬/K번째 수.java
@@ -1,20 +1,11 @@
-import java.util.Arrays;
+import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-       
-        for(int i=0;i<commands.length;i++){
-            int startIndex=commands[i][0];
-            int endIndex=commands[i][1];
-            int selectIndex=commands[i][2];
-            int[] splitArray=new int[endIndex-startIndex+1];
-            int index=0;
-            for(int j=startIndex-1;j<endIndex;j++){
-                splitArray[index]=array[j];
-                index++;
-            }
-            Arrays.sort(splitArray);
-            answer[i]=splitArray[selectIndex-1];
+        for(int i =0; i<commands.length; i++) {
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1,commands[i][1]);
+            Arrays.sort(temp);
+            answer[i]=temp[commands[i][2]-1];
         }
         return answer;
     }
