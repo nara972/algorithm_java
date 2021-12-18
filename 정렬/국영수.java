e1481e97bc2106089089543250fd6f32bbfb934e
nara972 <68370958+nara972@users.noreply.github.com>
2021-12-18 20:37:50 +0900

    Create 국영수.java

---
+++ 정렬/국영수.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+class Student implements Comparable<Student>{
+	String name;
+	int korean;
+	int math;
+	int english;
+  
+	public Student(String name, int korean, int math, int english) {
+		this.name = name;
+		this.korean = korean;
+		this.math = math;
+		this.english = english;
+	}
+  
+	@Override
+	public int compareTo(Student student) {
+		if(this.korean<student.korean) {
+			return 1;
+		}
+		else if(this.korean==student.korean) {
+			if(this.english>student.english) {
+				return 1;
+			}
+			else if(this.english==student.english) {
+				if(this.math<student.math){
+					return 1;
+				}
+				else if(this.math==student.math) {
+					if(this.name.compareTo(student.name)>0) {
+						return 1;
+					}
+				}
+			}
+		}
+		return -1;
+	}
+}
+
+public class Main
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String name;
+		int korean,math,english;
+		Student student; 
+		List<Student> slist=new ArrayList<Student>();
+		for(int i=0;i<n;i++) {
+			name=sc.next();
+			korean=sc.nextInt();
+			english=sc.nextInt();
+			math=sc.nextInt();
+			student=new Student(name,korean,math,english);
+			slist.add(student);
+		}
+		
+		Collections.sort(slist);
+		for(int i=0;i<n;i++) {
+			System.out.println(slist.get(i).name);
+		}
+	}
+
+}
